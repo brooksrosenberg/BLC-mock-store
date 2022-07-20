d6e0cfb20610616ceac0670e0f68607865b0318f
Brooks Rosenberg <bjtr@bu.edu>
2022-07-20 11:52:32 -0400

    created react app

---
+++ blc-store/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Working app</h1>
+      {/*header*/}
+      {/*home*/}
+    </div>
+  );
+}
+
+export default App;
